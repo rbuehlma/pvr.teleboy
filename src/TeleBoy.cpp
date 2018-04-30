df2a02349b8fbe04fcee147b17c5220b0e6013b5
Martin Willi <martin.willi@cloudguard.ch>
2018-04-30 22:37:26 +0200

    Extract series/episodeNumber from EPG data

--- src/TeleBoy.cpp
+++ src/TeleBoy.cpp
@@ -386,8 +386,10 @@ void TeleBoy::GetEPGForChannelAsync(int uniqueChannelId, time_t iStart,
       tag.iParentalRating = 0; /* not supported */
       tag.iStarRating = 0; /* not supported */
       tag.bNotify = false; /* not supported */
-      tag.iSeriesNumber = 0; /* not supported */
-      tag.iEpisodeNumber = 0; /* not supported */
+      tag.iSeriesNumber =
+          item.HasMember("serie_season") ? item["serie_season"].GetInt() : 0;
+      tag.iEpisodeNumber =
+          item.HasMember("serie_episode") ? item["serie_episode"].GetInt() : 0;
       tag.iEpisodePartNumber = 0; /* not supported */
       tag.strEpisodeName =
           item.HasMember("subtitle") ?
